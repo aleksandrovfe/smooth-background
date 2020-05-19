11a1552405f9fc829969ad823a4c072dda5df2cf
aleksandrovfe <aleksandrov7296@gmail.com>
2020-05-19 17:02:14 +0300

    implement firs img

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="img"></div>
+    </div>
+  );
+}
+
+
+export default App;
