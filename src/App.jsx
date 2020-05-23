49aabcdfe1a04a4736da8c04c8ea9d65e78fc7aa
aleksandrovfe <aleksandrov7296@gmail.com>
2020-05-23 15:21:23 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.scss";
 
+const classes = ['video', 'img1', 'img2', 'img3', 'img4', 'img5']
 
-function App() {
+const App = () => {
+  const [flag, setFlag] = useState(0)
+
+  const onChangeImg = (imageNumbert) => setFlag(imageNumbert)
+
   return (
     <div className="App">
-      <div className="img"></div>
+      <div className="sign1">У тебя очень красивая улыбка</div>
+      <div className="sign2">Давай наконец перейдем на ты</div>
+      <div className="sign3">Улыбайся чаще</div>
+      <div className="sign4">Повторяй слова</div>
+      <div className={`img ${classes[flag]}`}></div>
+      <video
+          src="assets/video.mp4"
+          className={`${classes[flag]}`}
+          loop="loop"
+          autoPlay="autoPlay"
+        >
+        </video>
+      <div className="buttons-list">
+        <button className='btn' onClick={() => onChangeImg(0)}></button>
+        <button className='btn' onClick={() => onChangeImg(1)}></button>
+        <button className='btn' onClick={() => onChangeImg(2)}></button>
+        <button className='btn' onClick={() => onChangeImg(3)}></button>
+        <button className='btn' onClick={() => onChangeImg(4)}></button>
+        <button className='btn' onClick={() => onChangeImg(5)}></button>
+      </div>
     </div>
   );
 }
